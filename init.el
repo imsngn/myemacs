1b10c31eca4e1780bd3d7d0a9d22ca401a252918
imsngn <wcdma@live.cn>
2017-12-13 13:40:27 +0800

    add load load-file

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(add-to-list 'load-path "~/.emacs.d/lisp/")
+;;(add-to-list 'load-path "~/.emacs.d/lisp/")
 
 ;- save your configure
 ;- define a function to quickly open your config file
@@ -20,11 +20,24 @@
 (defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
-(require 'init-packages )
-(require 'init-ui)
-(require 'init-better-defaults)
-(require 'init-keybindings)
-(require 'init-org)
+;;(require 'init-packages )
+;;(require 'init-ui)
+;;(require 'init-better-defaults)
+;;(require 'init-keybindings)
+;;(require 'init-org)
+
+;;(load "init-packages" )                 
+;;(load "init-ui")
+;;(load "init-better-defaults")
+;;(load "init-keybindings")
+;;(load "init-org")
+
+(load-file "~/.emacs.d/lisp/init-packages.elc")
+(load-file "~/.emacs.d/lisp/init-ui.elc")
+(load-file "~/.emacs.d/lisp/init-better-defaults.elc")
+(load-file "~/.emacs.d/lisp/init-keybindings.elc")
+(load-file "~/.emacs.d/lisp/init-org.elc")
+
 
 
 (setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
